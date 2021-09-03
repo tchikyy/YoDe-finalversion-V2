535fffe8d3c4e88a9231ea37db5c5c062816bcdc
tchikyy <76624013+tchikyy@users.noreply.github.com>
2021-09-03 06:25:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,26 @@ from werkzeug.utils import secure_filename
 import os
 import subprocess
 from flask_ngrok import run_with_ngrok
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 run_with_ngrok(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedBack.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class feedBack(db.Model):
+  id = db.Column(db.Integer,  primary_key=True)
+  text = db.Column(db.Text)
+  name = db.Column(db.Text)
+  email = db.Column(db.Text)
+
+  def __init(self, text, name, mail):
+    self.text = text
+    self.name = name
+    self.mail = mail
 
 uploads_dir = os.path.join(app.instance_path, 'uploads')
 
@@ -36,6 +53,24 @@ def predict():
     #return obj
         return render_template("index.html", source=os.path.join(get_direct, secure_filename(video.filename)))
 
+    if formid == 2:
+        name = request.form.get("y")
+        email = request.form.get("x")
+        text = request.form.get("z")
+
+        if name == '': 
+          return render_template("index.html", text="you forgot your name :(", color="red", scrollToAnchor="feed-back");
+        elif text == '':
+          return render_template("index.html", text="you forgot your opinion :(", color="red", scrollToAnchor="feed-back");
+        elif email == '':
+          return render_template("index.html", text="you forgot your email :(", color="red", scrollToAnchor="feed-back");
+        else:
+          feed_back = feedBack(name=name, email=email, text=text)
+          db.session.add(feed_back)
+          db.session.commit()
+
+          t="thnx for the feed-back :)"
+          return render_template("index.html", text=t, color="lime", scrollToAnchor="feed-back");    
 #@app.route('/return-files', methods=['GET'])
 #def return_file():
 #    obj = request.args.get('obj')
@@ -53,4 +88,5 @@ def predict():
 # 	return redirect(url_for('static/video_1.mp4', code=200))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run()
