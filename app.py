64f497d79dad585d41fd19f0dcd7039083189c11
tchikyy <76624013+tchikyy@users.noreply.github.com>
2021-09-03 06:06:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template, request, redirect, send_file, url_for
 from werkzeug.utils import secure_filename, send_from_directory
 import os
 import subprocess
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
 uploads_dir = os.path.join(app.instance_path, 'uploads')
 
@@ -26,21 +28,25 @@ def detect():
     subprocess.run(['python3', 'detect.py', '--source', os.path.join(uploads_dir, secure_filename(video.filename))])
 
     # return os.path.join(uploads_dir, secure_filename(video.filename))
-    obj = secure_filename(video.filename)
-    return obj
+    #obj = secure_filename(video.filename)
+    #return obj
+    return render_template("index.html", source=os.path.join(uploads_dir, secure_filename(video.filename)))
 
-@app.route('/return-files', methods=['GET'])
-def return_file():
-    obj = request.args.get('obj')
-    loc = os.path.join("runs/detect", obj)
-    print(loc)
-    try:
-        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
-        # return send_from_directory(loc, obj)
-    except Exception as e:
-        return str(e)
+#@app.route('/return-files', methods=['GET'])
+#def return_file():
+#    obj = request.args.get('obj')
+#    loc = os.path.join("runs/detect", obj)
+#    print(loc)
+#    try:
+#        return send_file(os.path.join("runs/detect", obj), attachment_filename=obj)
+#        # return send_from_directory(loc, obj)
+#    except Exception as e:
+#        return str(e)
 
 # @app.route('/display/<filename>')
 # def display_video(filename):
 # 	#print('display_video filename: ' + filename)
 # 	return redirect(url_for('static/video_1.mp4', code=200))
+
+if "__name__" == "__main__":
+    app.run()
